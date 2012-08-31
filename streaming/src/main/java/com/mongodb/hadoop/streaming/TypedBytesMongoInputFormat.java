28d851c42cd6991584b07bfd2dcaf610c9d918b2
Jon Eisen <jeisen@underarmour.com>
2012-08-31 13:23:10 -0400

    Added TypedBytesMongoInputFormat for dumbo use

---
+++ streaming/src/main/java/com/mongodb/hadoop/streaming/TypedBytesMongoInputFormat.java
@@ -0,0 +1,104 @@
+/*
+ * Copyright 2011 10gen Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.mongodb.hadoop;
+
+// Mongo
+
+import com.mongodb.*;
+import com.mongodb.hadoop.input.*;
+import com.mongodb.hadoop.util.*;
+import org.apache.commons.logging.*;
+import org.apache.hadoop.conf.*;
+import org.apache.hadoop.mapreduce.*;
+import org.apache.hadoop.typedbytes.TypedBytesWritable;
+import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.mapred.Reporter;
+import org.bson.*;
+
+import java.util.*;
+
+// Commons
+// Hadoop
+// Java
+
+/**
+* TypedBytesMongoInputFormat is a wrapper the MongoInputFormat that transforms all input/output into TypedBytes.
+* This is done so Dumbo can use it.
+*/
+public class TypedBytesMongoInputFormat extends InputFormat<TypedBytesWritable, TypedBytesWritable> {    
+    private static final Log LOG = LogFactory.getLog( MongoInputFormat.class );
+
+    @Override
+    public RecordReader<TypedBytesWritable, TypedBytesWritable> createRecordReader( InputSplit split, TaskAttemptContext context ){
+        if ( !( split instanceof MongoInputSplit ) )
+            throw new IllegalStateException( "Creation of a new RecordReader requires a MongoInputSplit instance." );
+
+        final MongoInputSplit mis = (MongoInputSplit) split;
+
+        return new TypedBytesMongoRecordReader(mis);
+    }
+
+    @Override
+    public List<InputSplit> getSplits( JobContext context ){
+        final Configuration hadoopConfiguration = context.getConfiguration();
+        final MongoConfig conf = new MongoConfig( hadoopConfiguration );
+        return MongoSplitter.calculateSplits( conf );
+    }
+
+    public class TypedBytesMongoRecordReader extends RecordReader<TypedBytesWritable, TypedBytesWritable> {
+        MongoRecordReader reader;
+        public TypedBytesMongoRecordReader(MongoInputSplit split) {
+            reader = new MongoRecordReader(split);
+        }
+
+        private TypedBytesWritable convertObj(Object obj) {
+            TypedBytesWritable tbw = new TypedBytesWritable();
+            tbw.setValue(obj);
+            return tbw;
+        }
+
+        @Override
+        public void close() {
+            reader.close();
+        }
+
+        @Override
+        public TypedBytesWritable getCurrentKey() {
+            return convertObj(reader.getCurrentKey());
+        }
+
+        @Override
+        public TypedBytesWritable getCurrentValue() {
+            return convertObj(reader.getCurrentValue());
+        }
+
+        @Override
+        public void initialize(InputSplit split, TaskAttemptContext context) {
+            reader.initialize(split, context);
+        }
+
+        @Override
+        public boolean nextKeyValue() {
+            return reader.nextKeyValue();
+        }
+
+        @Override
+        public float getProgress() {
+            return reader.getProgress();
+        }
+    }
+}
