407cac900d2814c1ed5785a68d7531f144d4b44a
Jon Eisen <jeisen@underarmour.com>
2012-09-04 10:16:58 -0400

    Moved back to .mapred because streaming requires old interface

--- streaming/src/main/java/com/mongodb/hadoop/streaming/TypedBytesMongoInputFormat.java
+++ streaming/src/main/java/com/mongodb/hadoop/streaming/TypedBytesMongoInputFormat.java
@@ -24,10 +24,12 @@ import com.mongodb.hadoop.input.*;
 import com.mongodb.hadoop.util.*;
 import org.apache.commons.logging.*;
 import org.apache.hadoop.conf.*;
-import org.apache.hadoop.mapreduce.*;
+import org.apache.hadoop.mapred.*;
+import org.apache.hadoop.mapreduce.InputSplit;
 import org.apache.hadoop.typedbytes.TypedBytesWritable;
 import org.bson.*;
 
+import java.io.*;
 import java.util.*;
 
 // Commons
@@ -38,65 +40,96 @@ import java.util.*;
 * TypedBytesMongoInputFormat is a wrapper the TypedBytesMongoInputFormat that transforms all input/output into TypedBytes.
 * This is done so Dumbo can use it.
 */
-public class TypedBytesMongoInputFormat extends InputFormat<TypedBytesWritable, TypedBytesWritable> {    
+public class TypedBytesMongoInputFormat implements InputFormat<TypedBytesWritable, TypedBytesWritable> {    
     private static final Log LOG = LogFactory.getLog( TypedBytesMongoInputFormat.class );
 
     @Override
-    public RecordReader<TypedBytesWritable, TypedBytesWritable> createRecordReader( InputSplit split, TaskAttemptContext context ){
-        if ( !( split instanceof MongoInputSplit ) )
-            throw new IllegalStateException( "Creation of a new RecordReader requires a MongoInputSplit instance." );
+    public RecordReader<TypedBytesWritable, TypedBytesWritable> getRecordReader( 
+        org.apache.hadoop.mapred.InputSplit split, 
+        JobConf job, 
+        Reporter reporter ) 
+    {
+        if ( !( split instanceof OldApiMongoInputSplit ) )
+            throw new IllegalStateException( "Creation of a new RecordReader requires a OldApiMongoInputSplit instance." );
 
-        final MongoInputSplit mis = (MongoInputSplit) split;
+        final OldApiMongoInputSplit mis = (OldApiMongoInputSplit) split;
 
-        return new TypedBytesMongoRecordReader(mis);
+        return new TypedBytesMongoRecordReader(mis.getBase());
     }
 
     @Override
-    public List<InputSplit> getSplits( JobContext context ){
-        final Configuration hadoopConfiguration = context.getConfiguration();
-        final MongoConfig conf = new MongoConfig( hadoopConfiguration );
-        return MongoSplitter.calculateSplits( conf );
+    public org.apache.hadoop.mapred.InputSplit[] getSplits( JobConf job, int numSplits ){
+        final MongoConfig conf = new MongoConfig( job );
+        List<org.apache.hadoop.mapreduce.InputSplit> list = MongoSplitter.calculateSplits( conf );
+        org.apache.hadoop.mapred.InputSplit[] arr = new org.apache.hadoop.mapred.InputSplit[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            if (!(list.get(i) instanceof MongoInputSplit)) {
+                throw new IllegalStateException("Calculating mongo splits should return a type of MongoInputSplit!");
+            }
+            arr[i] = new OldApiMongoInputSplit((MongoInputSplit) list.get(i));
+        }
+        return arr;
     }
 
-    public class TypedBytesMongoRecordReader extends RecordReader<TypedBytesWritable, TypedBytesWritable> {
-        private MongoRecordReader reader;
+    public class OldApiMongoInputSplit implements org.apache.hadoop.mapred.InputSplit {
+        MongoInputSplit base;
+        public OldApiMongoInputSplit(MongoInputSplit split) {
+            base = split;
+        }
+        public long getLength() {
+            return base.getLength();
+        }
+        public String[] getLocations() {
+            return base.getLocations();
+        }
+        public void readFields( DataInput in ) throws IOException {
+            base.readFields(in);
+        }
+        public void write (final DataOutput out) throws IOException {
+            base.write(out);
+        }
+        public MongoInputSplit getBase() {
+            return base;
+        }
+    }
+
+    public class TypedBytesMongoRecordReader implements RecordReader<TypedBytesWritable, TypedBytesWritable> {
+        MongoRecordReader reader;
+        long i;
 
         public TypedBytesMongoRecordReader(MongoInputSplit split) {
             reader = new MongoRecordReader(split);
+            reader.initialize(split, null);
+            i = 0;
         }
 
-        @Override
-        public void initialize(InputSplit split, TaskAttemptContext context) {
-            reader.initialize(split, context);
-        }
-
-        @Override
-        public boolean nextKeyValue() {
-            return reader.nextKeyValue();
-        }
-
-        @Override
-        public TypedBytesWritable getCurrentKey() {
-            TypedBytesWritable tbw = new TypedBytesWritable();
-            tbw.setValue(reader.getCurrentKey());
-            return tbw;
-        }
-
-        @Override
-        public TypedBytesWritable getCurrentValue() {
-            TypedBytesWritable tbw = new TypedBytesWritable();
-            tbw.setValue(reader.getCurrentValue());
-            return tbw;
-        }
-
-        @Override
-        public float getProgress() {
-            return reader.getProgress();
-        }
-
-        @Override
         public void close() {
             reader.close();
+        }
+
+        public boolean next(TypedBytesWritable key, TypedBytesWritable val) {
+            if (!reader.nextKeyValue())
+                return false;
+            i++;
+            key.setValue(reader.getCurrentKey());
+            val.setValue(reader.getCurrentValue());
+            return true;
+        }
+
+        public TypedBytesWritable createKey() {
+            return new TypedBytesWritable();
+        }
+
+        public TypedBytesWritable createValue() {
+            return new TypedBytesWritable();
+        }
+
+        public long getPos() {
+            return i;
+        }
+
+        public float getProgress() {
+            return reader.getProgress();
         }
     }
 }
